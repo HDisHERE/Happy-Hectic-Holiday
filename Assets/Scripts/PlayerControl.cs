cc8631c624ccfe830ed5c978a57d816a5d56bc54
HDisHERE <125623145+HDisHERE@users.noreply.github.com>
2024-03-05 01:48:26 -0500

    'Jump'

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -5,9 +5,15 @@ using UnityEngine;
 public class PlayerControl : MonoBehaviour
 {
     //Basic Data
+    //Walk
     Rigidbody2D rb;
     public float moveSpeed = 2.5f;
+
+    //Jump
+    [Range(0f, 30f)]
     public float jumpSpeed = 30.0f;
+    private bool isJumping=false;
+    private float x;
 
     private Transform groundTf;
     // Start is called before the first frame update
@@ -20,7 +26,14 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        x = Input.GetAxis("Horizontal");//Get input every frame.
+        isJumping = Input.GetButton("Jump");
+
+        if (isOnGround() && isJumping)
+        {
+            //rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);//One way for jumping. May change for optimization later
+            rb.velocity = Vector2.up * jumpSpeed;
+        }
     }
 
     private void FixedUpdate()
@@ -30,19 +43,15 @@ public class PlayerControl : MonoBehaviour
 
     public void PosUpdate()
     {
-        float x = Input.GetAxis("Horizontal");
-
         if (x != 0)
         {
             transform.localScale = new Vector3(Mathf.Sign(x), 1, 1);//Turn around the sprite. 
         }
 
-        if (isOnGround() && (Input.GetKey(KeyCode.UpArrow) || Input.GetKey(KeyCode.W)))
-        {
-            rb.velocity = new Vector2(rb.velocity.x, jumpSpeed);//One way for jumping. May change for optimization later
-        }
-
         rb.velocity = new Vector2(x * moveSpeed, rb.velocity.y);//Here I choose to change the velocity directly.
+        //Please notice that I changed the velocity directly instead of using force system. And there is a material
+        //That erase all the friction on the wall to avoid bug that stick player to the wall, which means that
+        //FORCE SYSTEM CAN'T BE USED TO CONTROL THE MOVEMENT IN THIS PROJECT. Otherwise everything is sliding.
     }
 
     public bool isOnGround()
