88198b233d09e3d1847d4c6c96e874bedbf6822a
CONALLJN <conall.nelligan@gmail.com>
2024-04-01 15:45:38 -0400

    Added Level Select Scenes and Gadget Select Scenes. Made changes to LevelEndScript

--- Assets/Scripts/BasicMechanics/LevelEndScript.cs
+++ Assets/Scripts/BasicMechanics/LevelEndScript.cs
@@ -5,14 +5,37 @@ using UnityEngine.SceneManagement;
 
 public class LevelEndScript : MonoBehaviour
 {
-    public string sceneTitle;
+    public string LevelSelect = "LevelSelect";
+    public string LevelSelect2 = "LevelSelect2";
+    public string LevelSelect3 = "LevelSelect3";
+    public static bool level1Complete = false;
+    public static bool level2Complete = false;
+    public int currentLevel;
 
     
     private void OnTriggerEnter2D(Collider2D collision)
     {
-        if(collision.gameObject.CompareTag("Player"))
+        if(collision.gameObject.CompareTag("Player") && level1Complete == false)
         {
-            SceneManager.LoadScene(sceneTitle);
+            if (this.currentLevel == 1)
+            {
+                level1Complete = true;
+            }
+            SceneManager.LoadScene(LevelSelect2);
+            Debug.Log("load");
         }
+        else if (collision.gameObject.CompareTag("Player") && level1Complete == true && level2Complete == false)
+        {
+            if (this.currentLevel == 2)
+            {
+                level2Complete = true;
+            }
+            SceneManager.LoadScene(LevelSelect3);
+        }
+        else if (collision.gameObject.CompareTag("Player") && level1Complete == true && level2Complete == true)
+        {
+            SceneManager.LoadScene(LevelSelect3);
+        }
+
     }
 }
